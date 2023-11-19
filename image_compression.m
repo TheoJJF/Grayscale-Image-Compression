39837cf5888ca999e0420c30d8202b0a7b2fe7c7
TheoJJF <131817438+TheoJJF@users.noreply.github.com>
2023-11-18 22:20:57 -0800

    Initial Commit

---
+++ image_compression.m
@@ -0,0 +1,16 @@
+A = imread(input('File path/name: ','s'));
+A = rgb2gray(A);
+
+A = double(A);
+[U,S,V] = svd(A);
+
+r = input('Target rank: ');
+if r < 1
+    r = 1;
+end
+
+approxA = uint8(U(:,1:r)*S(1:r,1:r)*V(:,1:r)');
+result_img = figure(1);
+imshow(approxA);
+
+saveas(result_img, compressed_result.jpg);
