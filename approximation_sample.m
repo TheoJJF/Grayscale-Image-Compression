2e6f9e2ffeafaf2e80cdc58034b0e8cea6dc4563
TheoJJF <131817438+TheoJJF@users.noreply.github.com>
2023-11-19 09:29:29 -0800

    Update sample amount

--- approximation_sample.m
+++ approximation_sample.m
@@ -4,7 +4,7 @@ A = rgb2gray(A);
 A = double(A);
 [U,S,V] = svd(A);
 
-ranks = [1 2 4 8 16 32 64 128 256 512 1024 2048];
+ranks = [1 50 100 500];
 l = length(ranks);
 
 for i=1:l
@@ -13,7 +13,8 @@ for i=1:l
     approxA = uint8(U(:,1:k) *S (1:k,1:k) * V(:,1:k)');
 
     figure(1);
-    subplot(3,4,i);
+    subplot(2,2,i);
+    
     imshow(approxA);
     title(sprintf('Rank %d Approximation', k))
 
